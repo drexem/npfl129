ed551fabeb515c75d0b6119f639f57ec9452a408
michal <michalkolcun1@gmail.com>
2025-10-27 15:21:59 +0100

    first impl

--- labs/04/mlp_classification_sgd.py
+++ labs/04/mlp_classification_sgd.py
@@ -5,6 +5,7 @@ import numpy as np
 import sklearn.datasets
 import sklearn.metrics
 import sklearn.model_selection
+from sklearn.preprocessing import OneHotEncoder
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
@@ -26,6 +27,11 @@ def main(args: argparse.Namespace) -> tuple[tuple[np.ndarray, ...], list[float]]
     # Load the digits dataset.
     data, target = sklearn.datasets.load_digits(n_class=args.classes, return_X_y=True)
 
+    target = target.reshape(-1,1)
+    enc = OneHotEncoder(handle_unknown='ignore')
+    enc.fit(target)
+    target = enc.transform(target).toarray()
+
     # Split the dataset into a train set and a test set.
     # Use `sklearn.model_selection.train_test_split` method call, passing
     # arguments `test_size=args.test_size, random_state=args.seed`.
@@ -38,21 +44,15 @@ def main(args: argparse.Namespace) -> tuple[tuple[np.ndarray, ...], list[float]]
     biases = [np.zeros(args.hidden_layer), np.zeros(args.classes)]
 
     def forward(inputs):
-        # TODO: Implement forward propagation, returning *both* the value of the hidden
-        # layer and the value of the output layer.
-        #
-        # We assume a neural network with a single hidden layer of size `args.hidden_layer`
-        # and ReLU activation, where $ReLU(x) = max(x, 0)$, and an output layer with softmax
-        # activation.
-        #
-        # The value of the hidden layer is computed as `ReLU(inputs @ weights[0] + biases[0])`.
-        # The value of the output layer is computed as `softmax(hidden_layer @ weights[1] + biases[1])`.
-        #
-        # Note that you need to be careful when computing softmax, because the exponentiation
-        # in softmax can easily overflow. To avoid it, you should use the fact that
-        # $softmax(z) = softmax(z + any_constant)$ and compute $softmax(z) = softmax(z - maximum_of_z)$.
-        # That way we only exponentiate values which are non-positive, and overflow does not occur.
-        raise NotImplementedError()
+        a = inputs @ weights[0] + biases[0]
+        hidden = np.maximum(a, 0)
+
+        logits = hidden @ weights[1] + biases[1]
+        logits_max = np.max(logits, axis=1, keepdims=True)
+        exp_logits = np.exp(logits - logits_max)
+        output = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
+
+        return a, hidden, output
 
     for epoch in range(args.epochs):
         permutation = generator.permutation(train_data.shape[0])
@@ -60,23 +60,41 @@ def main(args: argparse.Namespace) -> tuple[tuple[np.ndarray, ...], list[float]]
         # TODO: Process the data in the order of `permutation`. For every
         # `args.batch_size` of them, average their gradient, and update the weights.
         # You can assume that `args.batch_size` exactly divides `train_data.shape[0]`.
-        #
-        # The gradient used in SGD has now four parts, gradient of `weights[0]` and `weights[1]`
-        # and gradient of `biases[0]` and `biases[1]`.
-        #
-        # You can either compute the gradient directly from the neural network formula,
-        # i.e., as a gradient of $-log P(target | data)$, or you can compute
-        # it step by step using the chain rule of derivatives, in the following order:
-        # - compute the derivative of the loss with respect to *inputs* of the
-        #   softmax on the last layer,
-        # - compute the derivative with respect to `weights[1]` and `biases[1]`,
-        # - compute the derivative with respect to the hidden layer output,
-        # - compute the derivative with respect to the hidden layer input,
-        # - compute the derivative with respect to `weights[0]` and `biases[0]`.
+        for start in range(0, len(permutation), args.batch_size):
+            batch_indices = permutation[start:start + args.batch_size]
+            batch_data = train_data[batch_indices]
+            batch_target = train_target[batch_indices]
+
+            # Forward pass - get ALL intermediate values
+            a, hidden, output = forward(batch_data)
+
+            # Backward pass
+            dL_dSoftInputs = output - batch_target
+
+            # Output layer gradients
+            dL_dWo = hidden.T @ dL_dSoftInputs
+            dL_dBo = np.sum(dL_dSoftInputs, axis=0)
+
+            # Hidden layer gradients
+            dL_dh = dL_dSoftInputs @ weights[1].T
+            dL_da = dL_dh * (a > 0)
+
+            # Input layer gradients
+            dL_dWi = batch_data.T @ dL_da
+            dL_dBi = np.sum(dL_da, axis=0)
+
+            weights[1] -= args.learning_rate * dL_dWo / args.batch_size
+            biases[1] -= args.learning_rate * dL_dBo / args.batch_size
+            weights[0] -= args.learning_rate * dL_dWi / args.batch_size
+            biases[0] -= args.learning_rate * dL_dBi / args.batch_size
 
         # TODO: After the SGD epoch, measure the accuracy for both the
         # train test and the test set.
-        train_accuracy, test_accuracy = ...
+        train_pred = forward(train_data)[1]
+        test_pred = forward(test_data)[1]
+
+        train_accuracy = np.mean(np.argmax(train_pred, axis=1) == np.argmax(train_target, axis=1))
+        test_accuracy = np.mean(np.argmax(test_pred, axis=1) == np.argmax(test_target, axis=1))
 
         print("After epoch {}: train acc {:.1f}%, test acc {:.1f}%".format(
             epoch + 1, 100 * train_accuracy, 100 * test_accuracy))
--- labs/04/mnist_competition.py
+++ labs/04/mnist_competition.py
@@ -9,6 +9,12 @@ import urllib.request
 
 import numpy as np
 import numpy.typing as npt
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+
+import ssl
+
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
@@ -17,6 +23,7 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="mnist_competition.model", type=str, help="Model path")
+parser.add_argument("--grid_search", default=True, action="store_true", help="Perform grid search")
 
 
 class Dataset:
@@ -31,7 +38,9 @@ class Dataset:
                  url="https://ufal.mff.cuni.cz/~courses/npfl129/2526/datasets/"):
         if not os.path.exists(name):
             print("Downloading dataset {}...".format(name), file=sys.stderr)
-            urllib.request.urlretrieve(url + name, filename="{}.tmp".format(name))
+            ctx = ssl._create_unverified_context()
+            with urllib.request.urlopen(url + name, context=ctx) as resp, open("{}.tmp".format(name), "wb") as out:
+                out.write(resp.read())
             os.rename("{}.tmp".format(name), name)
 
         # Load the dataset, i.e., `data` and optionally `target`.
@@ -47,15 +56,73 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
         np.random.seed(args.seed)
         train = Dataset()
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = ...
+        # Normalize the data to 0-1 range
+        scaler = StandardScaler()
+        train_data = scaler.fit_transform(train.data / 255.0)
 
-        # If you trained one or more MLPs, you can use the following code
-        # to compress it significantly (approximately 12 times). The snippet
-        # assumes the trained `MLPClassifier` is in the `mlp` variable.
-        #   mlp._optimizer = None
-        #   for i in range(len(mlp.coefs_)): mlp.coefs_[i] = mlp.coefs_[i].astype(np.float16)
-        #   for i in range(len(mlp.intercepts_)): mlp.intercepts_[i] = mlp.intercepts_[i].astype(np.float16)
+        if args.grid_search:
+            # Perform grid search to find best hyperparameters
+            print("Performing grid search...", file=sys.stderr)
+
+            param_grid = {
+                'hidden_layer_sizes': [(128, 64), (256, 128), (128, 64, 32), (256, 128, 64)],
+                'activation': ['relu', 'tanh'],
+                'alpha': [0.0001, 0.001, 0.01],
+                'learning_rate_init': [0.001, 0.01]
+            }
+
+            mlp_base = MLPClassifier(
+                solver='adam',
+                max_iter=30,
+                random_state=args.seed,
+                verbose=False,
+                early_stopping=True,
+                validation_fraction=0.1,
+                n_iter_no_change=5
+            )
+
+            grid_search = GridSearchCV(
+                mlp_base,
+                param_grid,
+                cv=3,
+                verbose=2,
+                n_jobs=-1,
+                scoring='accuracy'
+            )
+
+            grid_search.fit(train_data, train.target)
+
+            print("\nBest parameters found:", file=sys.stderr)
+            print(grid_search.best_params_, file=sys.stderr)
+            print(f"Best cross-validation score: {grid_search.best_score_:.4f}", file=sys.stderr)
+
+            mlp = grid_search.best_estimator_
+        else:
+            # Train an MLP neural network with default parameters
+            mlp = MLPClassifier(
+                hidden_layer_sizes=(128, 64),
+                activation='relu',
+                solver='adam',
+                max_iter=100,
+                random_state=args.seed,
+                verbose=True,
+                early_stopping=True,
+                validation_fraction=0.1,
+                n_iter_no_change=10
+            )
+
+            print("Training neural network...", file=sys.stderr)
+            mlp.fit(train_data, train.target)
+
+        # Compress the model as suggested in the comments
+        mlp._optimizer = None
+        for i in range(len(mlp.coefs_)):
+            mlp.coefs_[i] = mlp.coefs_[i].astype(np.float16)
+        for i in range(len(mlp.intercepts_)):
+            mlp.intercepts_[i] = mlp.intercepts_[i].astype(np.float16)
+
+        # Store both the scaler and the model
+        model = {'scaler': scaler, 'mlp': mlp}
 
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
@@ -68,8 +135,9 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
 
-        # TODO: Generate `predictions` with the test set predictions.
-        predictions = ...
+        # Normalize test data and generate predictions
+        test_data = model['scaler'].transform(test.data / 255.0)
+        predictions = model['mlp'].predict(test_data)
 
         return predictions
 
