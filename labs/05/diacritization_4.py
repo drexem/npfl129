a9bd3e62eba60399e04d5d19f64d1de688168269
Michal Kolcun <michalkolcun1@gmail.com>
2025-11-15 17:02:37 +0100

    some attempt

--- labs/05/diacritization_4.py
+++ labs/05/diacritization_4.py
@@ -8,6 +8,7 @@ from typing import Optional
 import urllib.request
 
 import numpy as np
+import ssl
 from sklearn.neural_network import MLPClassifier
 
 parser = argparse.ArgumentParser()
@@ -21,6 +22,72 @@ parser.add_argument("--n", default=7, type=int, help="N-gram window size")
 
 
 class Dataset:
+    # ... existing code ...
+
+    def __init__(self,
+                 name="fiction-train.txt",
+                 url="https://ufal.mff.cuni.cz/~courses/npfl129/2526/datasets/"):
+        if not os.path.exists(name):
+            print("Downloading dataset {}...".format(name), file=sys.stderr)
+
+            # Create SSL context that doesn't verify certificates
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+
+            licence_name = name.replace(".txt", ".LICENSE")
+
+            # Use SSL context for downloads
+            with urllib.request.urlopen(url + licence_name, context=ssl_context) as response:
+                with open(licence_name, 'wb') as f:
+                    f.write(response.read())
+
+            with urllib.request.urlopen(url + name, context=ssl_context) as response:
+                with open("{}.tmp".format(name), 'wb') as f:
+                    f.write(response.read())
+
+            os.rename("{}.tmp".format(name), name)
+
+        with open(name, "r", encoding="utf-8-sig") as dataset_file:
+            self.target = dataset_file.read()
+        self.data = self.target.translate(self.DIA_TO_NODIA)
+
+
+class Dictionary:
+    def __init__(self,
+                 name="fiction-dictionary.txt",
+                 url="https://ufal.mff.cuni.cz/~straka/courses/npfl129/2526/datasets/"):
+        if not os.path.exists(name):
+            print("Downloading dataset {}...".format(name), file=sys.stderr)
+
+            # Create SSL context that doesn't verify certificates
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+
+            licence_name = name.replace(".txt", ".LICENSE")
+
+            # Use SSL context for downloads
+            with urllib.request.urlopen(url + licence_name, context=ssl_context) as response:
+                with open(licence_name, 'wb') as f:
+                    f.write(response.read())
+
+            with urllib.request.urlopen(url + name, context=ssl_context) as response:
+                with open("{}.tmp".format(name), 'wb') as f:
+                    f.write(response.read())
+
+            os.rename("{}.tmp".format(name), name)
+
+        # Load the dictionary to `variants`
+        self.variants = {}
+        with open(name, "r", encoding="utf-8-sig") as dictionary_file:
+            for line in dictionary_file:
+                nodia_word, *variants = line.rstrip("\n").split()
+                self.variants[nodia_word] = variants
+
+
+
+class Dataset:
     LETTERS_NODIA = "acdeeinorstuuyz"
     LETTERS_DIA = "áčďéěíňóřšťúůýž"
 
@@ -31,9 +98,22 @@ class Dataset:
                  url="https://ufal.mff.cuni.cz/~courses/npfl129/2526/datasets/"):
         if not os.path.exists(name):
             print("Downloading dataset {}...".format(name), file=sys.stderr)
+
+            # Create SSL context that doesn't verify certificates
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+
             licence_name = name.replace(".txt", ".LICENSE")
-            urllib.request.urlretrieve(url + licence_name, filename=licence_name)
-            urllib.request.urlretrieve(url + name, filename="{}.tmp".format(name))
+
+            with urllib.request.urlopen(url + licence_name, context=ssl_context) as response:
+                with open(licence_name, 'wb') as f:
+                    f.write(response.read())
+
+            with urllib.request.urlopen(url + name, context=ssl_context) as response:
+                with open("{}.tmp".format(name), 'wb') as f:
+                    f.write(response.read())
+
             os.rename("{}.tmp".format(name), name)
 
         with open(name, "r", encoding="utf-8-sig") as dataset_file:
@@ -57,14 +137,12 @@ class ContextWindowEncoder:
         return character.isalpha() and character in self.restorable_chars
 
     def extract_features(self, context_string):
-        """Extract feature vector from context window."""
         center_idx = self.half_window
         center_character = context_string[center_idx]
 
         if not self.should_process(center_character):
             return None
 
-        # Process left half
         left_chars = []
         word_boundary_hit = False
         for idx in range(center_idx - 1, -1, -1):
@@ -79,7 +157,6 @@ class ContextWindowEncoder:
 
         left_chars.reverse()
 
-        # Process right half
         right_chars = []
         word_boundary_hit = False
         for idx in range(center_idx + 1, len(context_string)):
@@ -92,10 +169,8 @@ class ContextWindowEncoder:
                     right_chars.append(' ')
                     word_boundary_hit = True
 
-        # Construct normalized context
         normalized = ''.join(left_chars) + center_character + ''.join(right_chars)
 
-        # Build one-hot vector
         feature_vec = np.zeros(len(normalized) * self.vocab_size, dtype=np.float32)
 
         for pos, char in enumerate(normalized):
@@ -107,13 +182,14 @@ class ContextWindowEncoder:
 
 class DiacriticPredictor:
 
-    def __init__(self, window_size, vocab, char_map, restorable_chars, random_state=42):
+    def __init__(self, window_size, vocab, char_map, restorable_chars, random_state=42, dict=None):
         self.encoder = ContextWindowEncoder(window_size, vocab, char_map, restorable_chars)
         self.window_size = window_size
         self.half_window = (window_size - 1) // 2
         self.vocab = vocab
         self.network = None
         self.random_state = random_state
+        self.dict = dict
 
     def prepare_training_examples(self, input_text, gold_text):
         pad_size = self.half_window
@@ -147,36 +223,74 @@ class DiacriticPredictor:
 
         print(f"Training neural network on {len(X)} examples...", file=sys.stderr)
         self.network = MLPClassifier(
-            hidden_layer_sizes=(512, 256),
+            hidden_layer_sizes=(64, 32),
             alpha=0.01,
-            max_iter=1000,
+            max_iter=50,
             random_state=self.random_state,
             verbose=True
         )
         self.network.fit(X, y)
         dev_predictions = self.predict_text(dev_input)
 
-        pred_words = dev_predictions.split()
-        gold_words = dev_gold.split()
-        correct_words = sum(1 for p, g in zip(pred_words, gold_words) if p == g)
-        word_accuracy = 100.0 * correct_words / len(gold_words) if gold_words else 0
+        correct_chars = sum(1 for p, g in zip(dev_predictions, dev_gold) if p == g)
+        total_chars = len(dev_gold)
+        char_accuracy = 100.0 * correct_chars / total_chars if total_chars > 0 else 0
 
-        print(f"Dev WORD accuracy: {word_accuracy:}%", file=sys.stderr)
+        print(f"Dev char accuracy: {char_accuracy:}%", file=sys.stderr)
 
     def predict_text(self, input_text):
         pad_size = self.half_window
         input_padded = ' ' * pad_size + input_text + ' ' * pad_size
         result = []
 
+        current_word = ''
+        current_word_nodia = ''
+
         for position in range(pad_size, len(input_padded) - pad_size):
+            curr_char = input_padded[position]
+
+            if not curr_char.isalpha():
+                if current_word_nodia and self.dict:
+                    if current_word_nodia in self.dict.variants:
+                        variants = self.dict.variants[current_word_nodia]
+                        if current_word in variants:
+                            result.append(current_word)
+                        else:
+                            result.append(variants[0])
+                    else:
+                        result.append(current_word)
+                elif current_word:
+                    result.append(current_word)
+
+                result.append(curr_char)
+
+                current_word = ''
+                current_word_nodia = ''
+                continue
+
             window = input_padded[position - pad_size:position + 1 + pad_size]
             feature_vector = self.encoder.extract_features(window)
 
             if feature_vector is not None:
                 prediction = self.network.predict(feature_vector.reshape(1, -1))
-                result.append(self.vocab[int(prediction)])
+                predicted_char = self.vocab[int(prediction)]
+                current_word += predicted_char
+                current_word_nodia += curr_char
             else:
-                result.append(input_padded[position])
+                current_word += curr_char
+                current_word_nodia += curr_char
+
+        if current_word_nodia and self.dict:
+            if current_word_nodia in self.dict.variants:
+                variants = self.dict.variants[current_word_nodia]
+                if current_word in variants:
+                    result.append(current_word)
+                else:
+                    result.append(variants[0])
+            else:
+                result.append(current_word)
+        elif current_word:
+            result.append(current_word)
 
         return ''.join(result)
 
@@ -194,24 +308,29 @@ def main(args: argparse.Namespace) -> Optional[str]:
     if args.predict is None:
         np.random.seed(args.seed)
         train_dataset = Dataset()
+        dictionary = Dictionary()
 
         predictor = DiacriticPredictor(
             window_size=n,
             vocab=alphabet,
             char_map={c: i for i, c in enumerate(alphabet)},
             restorable_chars=restorable,
-            random_state=args.seed
+            random_state=args.seed,
+            dict=dictionary
         )
 
         predictor.fit(train_dataset.data, train_dataset.target)
 
-        # Save model
-        model_package = {'predictor': predictor, 'window_size': n}
+
+        model_package = {
+            'predictor': predictor,
+            'window_size': n,
+            'dictionary': dictionary
+                         }
         with lzma.open(args.model_path, "wb") as file_out:
             pickle.dump(model_package, file_out)
 
     else:
-        # Prediction phase
         test_dataset = Dataset(args.predict)
 
         with lzma.open(args.model_path, "rb") as file_in:
